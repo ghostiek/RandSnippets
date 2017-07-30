f24616b860b8869f09419a166e160fe5d3254904
ghostiek <patricktawil1@hotmail.com>
2017-07-30 16:43:55 +0400

    Update get_values_from_header.py

--- Python/Pandas/get_values_from_header.py
+++ Python/Pandas/get_values_from_header.py
@@ -1,4 +1,6 @@
-"""Goes through a dataframe (df) and checks if any of its headers contain a certain substring. Returns a list"""
+"""Goes through a dataframe (df) and checks if any of its headers contain a certain substring. Returns a list
+Example use case: df.loc[index, get_values_from_header(df, str_contained)].plot(kind="bar")
+"""
 def get_values_from_header(df, str_contained):
     headers = []
     for header in list(df):
