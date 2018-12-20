08efd7162c1dae9b97e7e62be29dcd18855c693f
Patrick S. Tawil <patricktawil1@hotmail.com>
2018-12-20 12:04:48 +0800

    Create get_table_names.py

---
+++ Python/Pandas/get_table_names.py
@@ -0,0 +1,20 @@
+"""
+This will get all the table names from the database.
+
+Parameter: cursor
+Return: A list of tuples
+"""
+
+import sqlite3
+import pprint
+
+con = sqlite3.connect(...\databaseName.db")
+cursor = con.cursor()
+
+def get_table_names(cursor, display = True):
+  cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+  tables = cursor.fetchall()
+  if display:
+    pprint.pprint(tables)
+  return tables
+  
