2c6cda7a7404eae7d0de1eb98bb0e86f7ee40548
Patrick S. Tawil <patricktawil1@hotmail.com>
2019-01-11 17:02:01 +0800

    Update get_values_from_header.py

--- Python/Pandas/get_values_from_header.py
+++ Python/Pandas/get_values_from_header.py
@@ -1,15 +1,12 @@
 """Goes through a dataframe (df) and checks if any of its headers contain a certain substring. Returns a list
 Example use case: df.loc[index, get_values_from_header(df, str_contained)].plot(kind="bar")
 """
-def get_values_from_header(df, str_contained, case_sensitive = False):
+import re
+def get_values_from_header(df, regex_match):
     col_names = []
     headers = list(df)
     
-    if not case_sensitive:
-        str_contained = str_contained.lower()
-        headers = set(map(str.lower, headers))
-       
-    for header in list(df):
-        if str_contained in header:
+    for header in headers:
+        if re.match(regex_match, header):
             col_names.append(header)
     return col_names
