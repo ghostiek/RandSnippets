7e40445e9f7ae3589f865cec5b9062f7055e48d1
Patrick S. Tawil <patricktawil1@hotmail.com>
2019-01-11 13:52:52 +0800

    Added case sens parameter

--- Python/Pandas/get_values_from_header.py
+++ Python/Pandas/get_values_from_header.py
@@ -1,9 +1,15 @@
 """Goes through a dataframe (df) and checks if any of its headers contain a certain substring. Returns a list
 Example use case: df.loc[index, get_values_from_header(df, str_contained)].plot(kind="bar")
 """
-def get_values_from_header(df, str_contained):
-    headers = []
+def get_values_from_header(df, str_contained, case_sensitive = False):
+    col_names = []
+    headers = list(df)
+    
+    if not case_sensitive:
+        str_contained = str_contained.lower()
+        headers = set(map(str.lower, headers))
+       
     for header in list(df):
         if str_contained in header:
-            headers.append(header)
-    return headers
+            col_names.append(header)
+    return col_names
